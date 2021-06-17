ed282aea9bd7d3f722d3ed91b49f329707161fc4
joppajoe <joeacn08@gmail.com>
2021-06-16 22:06:32 -0400

    created sequelize connection

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var models = require('./models');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -37,5 +37,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+models.sequelize.sync().then(() => {
+  console.log("mesa miss you so much annie!!!!!")
+})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// query requests ***
+
+
+// Action: get lightsaber hilt(s)
+
+router.get('/hilt/:id', function(req,res,next) {
+  console.log('hilt')
+});
+
+// Action: get Kyber Crystal(s); color(s)
+
+router.get('/crystal/:id', function(req,res,next) {
+  console.log('crystal')
+});
+
+// const query = `SELECT * from `;
+
+// db.all(query, (err, row) => {
+//   if (err) throw err;
+//   console.log(row);
+// });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'jar-jar' });
+});
+
+
+// Get About page ***
+
+router.get('/about', function(req,res,next) {
+  res.render('about',{title:'about page'});
+});
+
+// Get Contact page ***
+
+router.get('/contact', function(req,res,next) {
+  res.render('contact',{title:'contact page'});
 });
 
 module.exports = router;
