e2ca82bf465bb3b842649ea3e0afa60c7cfffc35
joppajoe <joeacn08@gmail.com>
2021-06-29 22:09:15 -0400

    dependencies added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var models = require('./models');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sabersRouter = require('./routes/sabers');
@@ -15,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 models.sequelize.sync({ alter:true }).then(function () {
     console.log("Mesa sync'd up wit da DADABAAAASE")
